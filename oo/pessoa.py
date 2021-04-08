15fda8bcf3bd3460c17ed1c6d00c8560c8452218
Flávio Borges <flyfmb@hotmail.com>
2021-04-08 11:57:19 -0300

    Criados atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
@@ -14,11 +15,18 @@ if __name__ == '__main__':
     print(Pessoa.cumprimentar(luciano))
     print(id(luciano))
     print(luciano.cumprimentar())
-    print(luciano.nome)
-    print(luciano.idade)
+    print(luciano.nome)  # qual é o nome de luciano
+    print(luciano.idade)  # qual é a idade de luciano
     for filho in luciano.filhos:  # para cada filho em filhos de luciano
         print(filho.nome)
     luciano.sobrenome = 'Ramalho' # add atributo dinamicamente
     del luciano.filhos  # deletar atributos
+    luciano.olhos = 1
+    del luciano.olhos
     print(luciano.__dict__)  # para ver todos os atributos do luciano
     print(renzo.__dict__)   # para ver todos os atributos do renzo
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(renzo.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))
